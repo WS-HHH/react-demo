a5873c8dc9d1295b87630cfd28f8a2da9f19ed04
shuangshuangzi <1395363307@qq.com>
2022-05-24 21:57:38 +0800

    feat(src): feat useState,useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import DemoUseState from './pages/Hooks/DemoUseState';
+import DemoUseEffect from './pages/Hooks/DemoUseEffect';
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
+      {/* <h1 className="text-3xl font-bold underline">
         Hello world!
         test git
-      </h1>
+      </h1> */}
+
+      <DemoUseState />
+      <DemoUseEffect />
     </div>
   );
 }
